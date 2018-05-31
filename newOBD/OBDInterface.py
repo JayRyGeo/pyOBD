9a957d744c0cf0a1b8695f85c675bf8b1ee793de
JayRyGeo <jeremy.ryan.george@gmail.com>
2018-05-31 17:50:05 -0600

    Debugging

--- newOBD/OBDInterface.py
+++ newOBD/OBDInterface.py
@@ -1,5 +1,26 @@
 # This is my own adaptation of the pyOBD application
-import GetPorts
+from GetPorts import scanSerial as GetPorts
 from dualIO import WriteToLog
+import OBDConn
 import serial
 import time
+
+def Testing():
+    baudList = [9600, 38400, 19200, 57600, 115200]
+
+    portList = []
+    portList = GetPorts()
+
+    WriteToLog("Found ports: %s" % portList)
+    time.sleep(1)
+
+    if portList != None:
+        for port in portList:
+            for baud in baudList:
+                WriteToLog("Attemping %s with baud %s" % (port, baud))
+                time.sleep(1)
+                conn = OBDConn.OBDConn(port, baud)
+                time.sleep(1)
+
+
+Testing()
